e5b1b82597b7acbf09e6e13fcd59607cd34fef7e
Goran Kostic <damijan0034@gmail.com>
2023-08-21 11:07:31 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='uppercase flex justify-between p-5
+    text-red-400 font-bold md:py-10
+    '>
+      <h1>CHALABRCAK</h1>
+      <p>@All rights reserved</p>
+
+    </div>
   )
 }
